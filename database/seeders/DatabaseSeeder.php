2e3c9f0def276778e718818c136e411ab4da5557
AndreaNavas18 <karennavas333@gmail.com>
2024-06-06 17:33:21 -0500

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(RoleAndPermissionSeeder::class);
         $this->call(UsersSeeder::class);
+        $this->call(RoleAndPermissionSeeder::class);
 
         // \App\Models\User::factory(10)->create();
 
