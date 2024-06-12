d28f59763aeab408be179a9a890ccf092a40a0e1
AndreaNavas18 <karennavas333@gmail.com>
2024-06-12 11:53:18 -0500

    paginacion tenderos

--- resources/views/modules/admin/administrar.blade.php
+++ resources/views/modules/admin/administrar.blade.php
@@ -33,7 +33,28 @@
                 @endforeach
             </tbody>
         </table>
-    
+        <!-- Paginación personalizada -->
+        <div class="custom-pagination">
+            @if ($tenderos->lastPage() > 1)
+                <ul>
+                    @if ($tenderos->currentPage() > 1)
+                        <li><a href="{{ $tenderos->url(1) }}" aria-label="First">&laquo;&laquo;</a></li>
+                        <li><a href="{{ $tenderos->previousPageUrl() }}" aria-label="Previous">&laquo;</a></li>
+                    @endif
+        
+                    @for ($i = max(1, $tenderos->currentPage() - 2); $i <= min($tenderos->lastPage(), $tenderos->currentPage() + 2); $i++)
+                        <li class="{{ ($tenderos->currentPage() == $i) ? 'active' : '' }}">
+                            <a href="{{ $tenderos->url($i) }}">{{ $i }}</a>
+                        </li>
+                    @endfor
+        
+                    @if ($tenderos->currentPage() < $tenderos->lastPage())
+                        <li><a href="{{ $tenderos->nextPageUrl() }}" aria-label="Next">&raquo;</a></li>
+                        <li><a href="{{ $tenderos->url($tenderos->lastPage()) }}" aria-label="Last">&raquo;&raquo;</a></li>
+                    @endif
+                </ul>
+            @endif
+        </div>
     </div>
 @endsection
 {{-- 
